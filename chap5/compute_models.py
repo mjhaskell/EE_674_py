10813f7b5a172888422dff9cdcbb9c49070a924f
Mat Haskell <mhaskell9@gmail.com>
2019-02-14 19:09:52 -0700

    Working on state space. Might be right

--- chap5/compute_models.py
+++ chap5/compute_models.py
@@ -70,8 +70,13 @@ def compute_tf_model(mav, trim_state, trim_input):
     return T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r
 
 def compute_ss_model(mav, trim_state, trim_input):
+    euler_trim = euler_state(trim_state)
+    A = np.empty((13,12))
+    for x in range(len(euler_trim)):
+        A[:,x] = f_euler(mav,euler_trim,trim_input)
 
-     return A_lon, B_lon, A_lat, B_lat
+
+    return A_lon, B_lon, A_lat, B_lat
 
 def euler_state(x_quat):
     # convert state x with attitude represented by quaternion
@@ -98,13 +103,64 @@ def quaternion_state(x_euler):
 
     return x_quat
 
-def f_euler(mav, x_euler, input):
+def dtheta_dq(quat):
+    h = 0.005
+    Jacobian = np.zeros((3,4))
+    for i in range(4):
+        q_p = np.copy(quat)
+        q_p[i][0] += h
+        q_m = np.copy(quat)
+        q_m[i][0] -= h
+
+        phi,theta,psi = Quaternion2Euler(q_p)
+        f_p = np.array([[phi,theta,psi]]).T
+        
+        phi,theta,psi = Quaternion2Euler(q_m)
+        f_m = np.array([[phi,theta,psi]]).T
+
+        Jac_col_i = (f_p - f_m) / (2 * h)
+        Jacobian[:,i] = Jac_col_i[:,0]
+
+    return Jacobian
+
+def f_euler(mav, x_euler, delta):
     # return 12x1 dynamics (as if state were Euler state)
     # compute f at euler_state
+    u = mav._forces_moments(delta)
+    x_quat = quaternion_state(x_euler)
+    f_euler_ = mav._derivatives(x_quat,u)
+
     return f_euler_
 
-def df_dx(mav, x_euler, input):
+def df_dx(mav, x_euler, delta):
     # take partial of f_euler with respect to x_euler
+    x_quat = quaternion_state(x_euler)
+    dT_dx = np.zeros((12,13))
+    dT_dx[:6,:6] = np.eye(6)
+    dT_dx[9:12,10:13] = np.eye(3)
+    dT_dx[6:9,6:10] = dtheta_dq(x_quat[6:10])
+
+#    u = mav._forces_moments(delta)
+    
+    h = 0.005
+    Jacobian = np.zeros((13,12))
+    for i in range(12):
+        x_p = np.copy(x_euler)
+        x_p[i][0] += h
+        x_m = np.copy(x_euler)
+        x_m[i][0] -= h
+
+#        f_p = mav._derivatives(x_p,u)
+#        f_m = mav._derivatives(x_m,u)
+
+        f_p = f_euler(mav,x_p,delta)
+        f_m = f_euler(mav,x_m,delta)
+
+        Jac_col_i = (f_p - f_m) / (2 * h)
+        Jacobian[:,i] = Jac_col_i[:,0]
+
+    A = dT_dx @ Jacobian
+    
     return A
 
 def df_du(mav, x_euler, delta):
@@ -139,21 +195,37 @@ if __name__ == "__main__":
     trim_state, trim_input = compute_trim(dyn, Va, gamma)
     dyn._state = trim_state
 
-    T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, \
-    T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r \
-        = compute_tf_model(dyn, trim_state, trim_input)
-    print('T_phi_delta_a: \n',T_phi_delta_a)
-    print('T_chi_phi: \n',T_chi_phi)
-    print('T_theta_delta_e: \n',T_theta_delta_e)
-    print('T_h_theta: \n',T_h_theta)
-    print('T_h_Va: \n',T_h_Va)
-    print('T_Va_delta_t: \n',T_Va_delta_t)
-    print('T_Va_theta: \n',T_Va_theta)
-    print('T_beta_delta_r: \n',T_beta_delta_r)
+#    T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, \
+#    T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r \
+#        = compute_tf_model(dyn, trim_state, trim_input)
+#    print('T_phi_delta_a: \n',T_phi_delta_a)
+#    print('T_chi_phi: \n',T_chi_phi)
+#    print('T_theta_delta_e: \n',T_theta_delta_e)
+#    print('T_h_theta: \n',T_h_theta)
+#    print('T_h_Va: \n',T_h_Va)
+#    print('T_Va_delta_t: \n',T_Va_delta_t)
+#    print('T_Va_theta: \n',T_Va_theta)
+#    print('T_beta_delta_r: \n',T_beta_delta_r)
 
-    q = np.array([[1,0,0,0]]).T
-    x_e = np.array([[1,2,3,4,5,6,0,np.pi/6.0,0,11,12,13]]).T
-    x_q = quaternion_state(x_e)
-    print('quat: \n',x_q)
-    x_e_test = euler_state(x_q)
-    print('euler: \n',x_e_test)
+#    q = np.array([[1,0,0,0]]).T
+#    x_e = np.array([[1,2,3,4,5,6,0,np.pi/6.0,0,11,12,13]]).T
+#    x_q = quaternion_state(x_e)
+#    print('quat: \n',x_q)
+#    x_e_test = euler_state(x_q)
+#    print('euler: \n',x_e_test)
+
+#    angle = np.pi / 6.0
+#    axis = np.array([[0,1,0]]).T
+#    q0 = np.cos(angle/2.0)
+#    q1 = np.sin(angle/2.0)*axis.item(0)
+#    q2 = np.sin(angle/2.0)*axis.item(1)
+#    q3 = np.sin(angle/2.0)*axis.item(2)
+#    quat = np.array([[q0,q1,q2,q3]]).T
+#    A = dtheta_dq(quat)
+#    print('dTheta_dQuat: \n',A)
+
+    x_euler = np.array([[2,0,-2,1,0,1,0,np.pi/4,0,0,0,0]]).T
+    delta = np.array([[-0.1,0.7,trim_input.item(2),trim_input.item(3)]]).T
+    wrench = dyn._forces_moments(delta)
+    A = df_dx(dyn,x_euler,wrench)
+    print('df_dx: \n',A)
