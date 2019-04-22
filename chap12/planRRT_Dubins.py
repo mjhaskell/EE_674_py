18cd679b7b7ba2eeed895e5f71687849c2b27cdb
Mat Haskell <mhaskell9@gmail.com>
2019-04-22 08:45:31 -0600

    Added code to check for collisions along dubins path, not working yet

--- chap12/planRRT_Dubins.py
+++ chap12/planRRT_Dubins.py
@@ -10,21 +10,27 @@ class planRRT():
         self.dubins_path = dubins_params()
 
     def planPath(self, wpp_start, wpp_end, R_min, map):
-        self.segmentLength = 2.5*R_min
+        self.segmentLength = 3.5*R_min
 
         # desired down position is down position of end node
         pd = wpp_end.item(2)
 
         # specify start and end nodes from wpp_start and wpp_end
         # format: N, E, D, chi, cost, parentIndex, connectsToGoalFlag,
-        start_node = np.array([wpp_start.item(0), wpp_start.item(1), pd, 0, 0, -1, 0])
-        end_node = np.array([wpp_end.item(0), wpp_end.item(1), pd, 0, 0, 0, 0])
+        start_node = np.array([wpp_start.item(0), wpp_start.item(1), pd, \
+                mod(wpp_end.item(4)), 0, -1, 0])
+        v = wpp_end[:2] - wpp_start[:2]
+        chi = np.arctan2(v.item(1),v.item(0))
+        end_node = np.array([wpp_end.item(0), wpp_end.item(1), pd, \
+                mod(chi), 0, 0, 0])
 
         # establish tree starting with the start node
         tree = start_node.reshape(1,len(start_node))
 
         # check to see if start_node connects directly to end_node
-        if ((np.linalg.norm(start_node[0:3] - end_node[0:3]) < self.segmentLength ) and not self.collision(start_node, end_node, map)):
+        if ((np.linalg.norm(start_node[0:3] - end_node[0:3]) < self.segmentLength )
+          and not self.collision(start_node, end_node, map)
+          and np.linalg.norm(start_node[:3]-end_node[:3]) >= 3*R):
             waypoints = msg_waypoints()
             waypoints.ned[:,0] = start_node[:3]
             waypoints.ned[:,1] = end_node[:3]
@@ -48,10 +54,13 @@ class planRRT():
         pt = np.hstack([north_east, pd])
         return pt
 
-    def collision(self, start_node, end_node, map, radius):
-        self.dubins_path.update(start_node[:3], start_node.item(3), \
-                end_node[:3], end_node.item(3), radius)
-        pts = self.pointsAlongPath(start_node, end_node, 5)
+    def collision(self, start_node, end_node, map, R):
+        self.dubins_path.update(start_node[:3].reshape((3,1)), start_node.item(3), \
+                end_node[:3].reshape((3,1)), end_node.item(3), R)
+
+        delta = 10
+        d_ang = np.radians(1)
+        pts = self.pointsAlongPath(d_ang, delta)
 
         for i in range(len(map.building_north)):
             norths = np.abs(pts[:,0] - map.building_north[i])
@@ -68,17 +77,103 @@ class planRRT():
                             return True
         return False
 
-    def pointsAlongPath(self, start_node, end_node, Del):
-        q = end_node[:3] - start_node[:3]
-        L = np.linalg.norm(q)
-        num_pts = int(np.ceil(L / Del))
-        q /= L
-        ned = np.zeros((num_pts,3))
+#    def pointsAlongPath(self, start_node, end_node, Del):
+#        q = end_node[:3] - start_node[:3]
+#        L = np.linalg.norm(q)
+#        num_pts = int(np.ceil(L / Del))
+#        q /= L
+#        ned = np.zeros((num_pts,3))
+#
+#        for i in range(num_pts):
+#            ned[i,:] = start_node[:3]+q*Del*(i+1)
+#
+#        return ned
 
-        for i in range(num_pts):
-            ned[i,:] = start_node[:3]+q*Del*(i+1)
+    def pointsAlongPath(self, Del_th, Del_lin):
+        # points along start circle
+        initialize_points = True
+        th1 = np.arctan2(self.dubins_params.p_s.item(1) - self.dubins_params.center_s.item(1),
+                        self.dubins_params.p_s.item(0) - self.dubins_params.center_s.item(0))
+        th1 = mod(th1)
+        th2 = np.arctan2(self.dubins_params.r1.item(1) - self.dubins_params.center_s.item(1),
+                         self.dubins_params.r1.item(0) - self.dubins_params.center_s.item(0))
+        th2 = mod(th2)
+        th = th1
+        theta_list = [th]
+        if self.dubins_params.dir_s > 0:
+            if th1 >= th2:
+                while th < th2 + 2*np.pi:
+                    th += Del_th
+                    theta_list.append(th)
+            else:
+                while th < th2:
+                    th += Del_th
+                    theta_list.append(th)
+        else:
+            if th1 <= th2:
+                while th > th2 - 2*np.pi:
+                     th -= Del_th
+                     theta_list.append(th)
+            else:
+                while th > th2:
+                    th -= Del_th
+                    theta_list.append(th)
 
-        return ned
+        if initialize_points:
+            points = np.array([[self.dubins_params.center_s.item(0) + self.dubins_params.radius * np.cos(theta_list[0]),
+                                self.dubins_params.center_s.item(1) + self.dubins_params.radius * np.sin(theta_list[0]),
+                                self.dubins_params.center_s.item(2)]])
+            initialize_points = False
+        for angle in theta_list:
+            new_point = np.array([[self.dubins_params.center_s.item(0) + self.dubins_params.radius * np.cos(angle),
+                                   self.dubins_params.center_s.item(1) + self.dubins_params.radius * np.sin(angle),
+                                   self.dubins_params.center_s.item(2)]])
+            points = np.concatenate((points, new_point), axis=0)
+
+        # points along straight line
+        vec = self.dubins_params.r2 - self.dubins_params.r1
+        v = np.linalg.norm(vec)
+        num_pts = np.ceil(v/Del_lin)
+        # Pdb().set_trace()
+        vec = vec/v
+        for i in range(int(num_pts)):
+            new_point = self.dubins_params.r1 + i * Del_lin * vec
+            points = np.concatenate((points, new_point.T), axis=0)
+
+        # points along end circle
+        th2 = np.arctan2(self.dubins_params.p_e.item(1) - self.dubins_params.center_e.item(1),
+                         self.dubins_params.p_e.item(0) - self.dubins_params.center_e.item(0))
+        th2 = mod(th2)
+        th1 = np.arctan2(self.dubins_params.r2.item(1) - self.dubins_params.center_e.item(1),
+                         self.dubins_params.r2.item(0) - self.dubins_params.center_e.item(0))
+        th1 = mod(th1)
+        th = th1
+        theta_list = [th]
+        if self.dubins_params.dir_e > 0:
+            if th1 >= th2:
+                while th < th2 + 2 * np.pi:
+                    th += Del_th
+                    theta_list.append(th)
+            else:
+                while th < th2:
+                    th += Del_th
+                    theta_list.append(th)
+        else:
+            if th1 <= th2:
+                while th > th2 - 2 * np.pi:
+                    th -= Del_th
+                    theta_list.append(th)
+            else:
+                while th > th2:
+                    th -= Del_th
+                    theta_list.append(th)
+        for angle in theta_list:
+            new_point = np.array([[self.dubins_params.center_e.item(0) + self.dubins_params.radius * np.cos(angle),
+                                   self.dubins_params.center_e.item(1) + self.dubins_params.radius * np.sin(angle),
+                                   self.dubins_params.center_e.item(2)]])
+            points = np.concatenate((points, new_point), axis=0)
+
+        return points
 
     def extendTree(self, tree, end_node, map, pd, radius):
         success = False
@@ -150,6 +245,10 @@ class planRRT():
             waypoints.flag_connect_to_goal[0][k] = node[5]
             k += 1
 
+        wayponits.type = 'dubins'
         waypoints.flag_wrap_waypoints = False
         waypoints.num_waypoints = k
         return waypoints
+
+def mod(x):
+    return x % (2*np.pi)
